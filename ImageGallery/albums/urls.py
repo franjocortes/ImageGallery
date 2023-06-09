6d9d2231e5d656879e075c0680329bdca075db8e
Jose Francisco Cortes <franjo.cortes@gmail.com>
2023-06-09 14:49:07 -0300

    add albums from modal and visualize in a table

--- ImageGallery/albums/urls.py
+++ ImageGallery/albums/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from albums.views import index
+from albums.views import index, create
 
 
 app_name = 'albums'
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('create/', create, name='create'),
 ]
--- ImageGallery/albums/views.py
+++ ImageGallery/albums/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from albums.forms import AlbumForm
+from albums.models import Album
 
-# Create your views here.
 
 def index(request):
     form = AlbumForm()
+    albums = Album.objects.all()
     context = {
         'title': 'Gallery',
         'form': form,
+        'albums': albums,
     }
     return render(request, 'albums/list.html', context)
+
+
+def create(request):
+    form = AlbumForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        album = form.save()
+        return redirect('albums:index')
